4b107bf4383d6ac9c507e0ac9d5166dc7feae74c
yb <lojobs38@gmail.com>
2022-03-29 17:01:59 +0800

    chore: add comment to launch.sh

--- launch.sh
+++ launch.sh
@@ -4,11 +4,23 @@
 # This script expects the standdard JMeter command parameters.
 #
 
+#  -e  Exit immediately if a command exits with a non-zero status.
+# https://stackoverflow.com/a/19622569/8955356
 set -e
+
+# ref: https://stackoverflow.com/a/15517399/8955356
+# -Xms<size>        set initial Java heap size
+
 freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
-s=$(($freeMem/10*8))
+
+# -Xss<size>        set java thread stack size
+s=$(($freeMem/10*8)) 
+
+# -Xmx<size>        set maximum Java heap size
 x=$(($freeMem/10*8))
-n=$(($freeMem/10*2))
+
+# -Xmn<size>        sets the initial and maximum size (in bytes) of the heap for the young generation (nursery)
+n=$(($freeMem/10*2)) 
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
 echo "START Running Apache JMeter on `date`"
