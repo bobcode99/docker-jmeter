7beca53a7ec655347872d1731a52d6c98fdb66d8
bob <lojobs38@gmail.com>
2022-11-25 15:22:16 +0800

    Using chrome107 base img

--- run-jmeter-chrome.sh
+++ run-jmeter-chrome.sh
@@ -4,7 +4,7 @@ timestamp=$(date +%Y%m%d_%H%M%S)
 volume_path=$(pwd)/share_folder
 jmeter_path=/mnt/jmeter
 TEST_NET=jmeter-distributed-network
-docker_image_name=jmeter-chrome-debug:5.5
+docker_image_name=jmeter-chrome:5.5
 test_plan_path_name=tests/selenium_test_plan.jmx
 
 docker run -it -v "${volume_path}":${jmeter_path} \
