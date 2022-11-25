61c967369f5af3cd0ace5dd2781479d0f393804b
bob <lojobs38@gmail.com>
2022-11-25 14:23:32 +0800

    Update run script

--- run-jmeter-chrome.sh
+++ run-jmeter-chrome.sh
@@ -4,15 +4,19 @@ timestamp=$(date +%Y%m%d_%H%M%S)
 volume_path=$(pwd)/share_folder
 jmeter_path=/mnt/jmeter
 TEST_NET=jmeter-distributed-network
-docker_image_name=jmeter-chrome:5.5
-test_plan_path_name=tests/test_plan_k6_test.jmx
+docker_image_name=jmeter-chrome-debug:5.5
+test_plan_path_name=tests/selenium_test_plan.jmx
 
 docker run -it -v "${volume_path}":${jmeter_path} \
 	--rm \
 	${docker_image_name} \
-    /bin/bash
+  -n \
+  -t ${jmeter_path}/${test_plan_path_name} \
+  -l ${jmeter_path}/client/result_${timestamp}.csv \
+  -j ${jmeter_path}/client/jmeter_${timestamp}.log \
+  -e -o ${jmeter_path}/client/html-report_${timestamp}
 
-# jmeter_path=/mnt/jmeter
+
 # jmeter -n -t ${jmeter_path}/${test_plan_path_name} \
 #   -l ${jmeter_path}/client/result_${timestamp}.csv \
 #   -j ${jmeter_path}/client/jmeter_${timestamp}.log \
